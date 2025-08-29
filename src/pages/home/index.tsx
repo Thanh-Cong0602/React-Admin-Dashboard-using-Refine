be7396432251a1316ca43e5a71f0e52b65d9e80a
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2025-08-29 23:08:07 +0700

    feat: completed UI UpcomingEvents in homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,17 @@
+import DealCharts from '@/components/home/deal-charts';
+import UpcomingEvents from '@/components/home/upcoming-events';
+import { Col, Row } from 'antd';
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Row gutter={[32, 32]} style={{ marginTop: '32px' }}>
+      <Col xs={24} sm={24} xl={8} style={{ height: '460px' }}>
+        <UpcomingEvents />
+      </Col>
+
+      <Col xs={24} sm={24} xl={8} style={{ height: '460px' }}>
+        <DealCharts />
+      </Col>
+    </Row>
+  );
 };
