0fe4616f5e576c9052e2e28f7638a73f36caf8ca
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2025-08-29 23:51:50 +0700

    feat: completed UI dashboard in homepage

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,5 +1,19 @@
 import gql from 'graphql-tag';
 
+export const DASHBOARD_TOTAL_COUNTS_QUERY = gql`
+  query DashboardTotalCounts {
+    companies {
+      totalCount
+    }
+    contacts {
+      totalCount
+    }
+    deals {
+      totalCount
+    }
+  }
+`;
+
 export const USERS_SELECT_QUERY = gql`
   query UsersSelect($filter: UserFilter!, $sorting: [UserSort!], $paging: OffsetPaging!) {
     users(filter: $filter, sorting: $sorting, paging: $paging) {
